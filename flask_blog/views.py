5300fa41bc431e6f4c47b986614d3c0a5f384d5f
yoshinami(note) <hokkaido.1993@gmail.com>
2019-09-03 22:18:32 +0900

    [ゼロからFlaskがよくわかる本] (P:282)

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -1,7 +1,8 @@
 #flask-blog/views.py
 
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
 @app.route('/')
 def show_entries():
-    return "Hello World"
+    return render_template('./entries/index.html')
